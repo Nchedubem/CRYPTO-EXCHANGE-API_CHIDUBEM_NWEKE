d0d07b7b580634e5c828f42d15995e009bc57137
Chidubem <nwekedubemph@gmail.com>
2023-09-07 05:10:20 -0700

    second commit

--- tasks/task_one.js
+++ tasks/task_one.js
@@ -1,18 +1,43 @@
-fetch('https://api.coincap.io/v2/assets')
-  .then(response => response.json())
-  .then(data => {
-    const assets = data.data.slice(0, 5); // Get the top 5 cryptocurrencies
+const cryptoElement = document.createElement('div');
+cryptoElement.innerHTML = `${rank}. ${name} (${symbol}): $${priceUsd}`;
 
-    const cryptoDataElement = document.getElementById('cryptoData');
-    assets.forEach(asset => {
-      const { id, rank, name, symbol, priceUsd } = asset;
+const cryptoDataElement = document.createElement('div');
+cryptoDataElement.id = 'cryptoData';
+document.body.appendChild(cryptoDataElement);
 
-      const cryptoElement = document.createElement('div');
-      cryptoElement.innerHTML = `<p>${rank}. ${name} (${symbol}): $${priceUsd}</p>`;
+const { id, rank, name, symbol, priceUsd } = asset;
 
-      cryptoDataElement.appendChild(cryptoElement);
+const cryptoElements = _.map(assets, (asset) => {
+  const { id, rank, name, symbol, priceUsd } = asset;
+  return `<p>${rank}. ${name} (${symbol}): $${priceUsd}</p>`;
+});
+
+_.each(cryptoElements, (element) => {
+  document.body.appendChild(element);
+});
+
+
+try {
+  fetch('https://api.coincap.io/v2/assets')
+    .then(response => response.json())
+    .then(data => {
+      const assets = data.data.slice(0, 5);
+
+      const cryptoDataElement = document.getElementById('cryptoData');
+      assets.forEach((asset) => {
+        const { id, rank, name, symbol, priceUsd } = asset;
+
+        const cryptoElement = document.createElement('div');
+        cryptoElement.innerHTML = `${rank}. ${name} (${symbol}): $${priceUsd}`;
+
+        cryptoDataElement.appendChild(cryptoElement);
+      });
+    })
+    .catch((error) => {
+      console.error('Error:', error);
+      alert('An error occurred while retrieving crypto data. Please try again later.');
     });
-  })
-  .catch(error => {
-    console.error('Error:', error);
-  });
+} catch (error) {
+  console.error('Error:', error);
+  alert('An error occurred while retrieving crypto data. Please try again later.');
+}
